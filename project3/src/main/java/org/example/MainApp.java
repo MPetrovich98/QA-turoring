801a45556561d40f96a261175452b7c94597dbd9
Milan Petrović <milannpetrovic98@gmail.com>
2022-07-05 20:26:07 +0200

    Sixth and seventh task don't work.
    It doesn't work because of ads.

---
+++ project3/src/main/java/org/example/MainApp.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import dev.failsafe.internal.util.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.apache.camel.component.dataset.DataSetEndpoint.assertEquals;
+
+/**
+ * A Camel Application
+ */
+public class MainApp {
+
+    /**
+     * A main() so we can easily run these routing rules in our IDE
+     */
+
+    public static void main(String[] args) throws InterruptedException {
+        Selenium selenium = new Selenium();
+        selenium.printUrl();
+        System.out.println("First three tasks done.\n\n\n");
+        Thread.sleep(5000);
+
+        WebElement usrname = selenium.getDriver().findElement(By.name("userName"));
+        usrname.sendKeys("pera");
+        WebElement password = selenium.getDriver().findElement(By.name("password"));
+        password.sendKeys("pera");
+        WebElement login =selenium.getDriver().findElement(By.name("submit"));
+        login.click();
+        String expectedUrl="https://demo.guru99.com/test/newtours/login_sucess.php";
+        selenium.setUrl(selenium.getDriver().getCurrentUrl());
+        String actualUrl= selenium.getUrl();
+        assertEquals(actualUrl, expectedUrl);
+        Thread.sleep(5000);
+
+        System.out.println("fourth and fifth tasks done.\n\n\n");
+        WebElement link= selenium.getDriver().findElement(By.linkText("Destinations"));
+        link.click();
+        selenium.setUrl(selenium.getDriver().getCurrentUrl());
+        actualUrl= selenium.getUrl();
+        assertEquals(actualUrl, "https://demo.guru99.com/test/newtours/destination.php");
+        selenium.printUrl();
+        Thread.sleep(5000);
+        System.out.println("sixth and seventh tasks done.\n\n\n");
+
+        selenium.getDriver().quit();
+    }
+
+    private static void assertEquals(String actualUrl, String expectedUrl) {
+        if (actualUrl.contentEquals(expectedUrl)){
+            System.out.println("Test Passed!");
+            System.out.println("expected url is "+expectedUrl);
+            System.out.println("actual url is "+actualUrl);
+        } else {
+            System.out.println("Test Failed");
+            System.out.println("expected url is "+expectedUrl);
+            System.out.println("expected url is "+actualUrl);
+        }
+    }
+
+}
+
